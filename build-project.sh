78fe6874b2bba9bfe03d63fee9e5993c8e9ba7ea
Peter Lobo <akumaokami10@gmail.com>
2018-04-19 08:45:36 -0500

    Update build-project.sh

--- build-project.sh
+++ build-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-protoc -I ./mcs --go_out=plugins=grpc:./pb ./mcs/*.proto
+#protoc -I ./mcs --go_out=plugins=grpc:./pb ./mcs/*.proto
+protoc  ./mcs/rpcserviceproto.proto --go_out=plugins=grpc:./
 
 eval $(minikube docker-env)
 
